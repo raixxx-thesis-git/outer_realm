022da85b0f8d9a75145ea1fbb4eb17558a4e35a6
raixxx-thesis-git <rai.machine.learning.explore@gmail.com>
2024-08-08 15:11:55 +0700

    update: adding 1 or 3 channel option in the tfr reading

--- initium.py
+++ initium.py
@@ -13,28 +13,33 @@ from outer_realm.assertor import Assertor
     tfrecords.
 '''
 @tf.function
-def _map_reader(bin: SymbolicTensor) -> (SymbolicTensor, SymbolicTensor):
+def _map_reader(three_channel: bool):
   # complying with the tfrecord structure
-  config = {
-    'data': tf.io.FixedLenFeature([], tf.string),
-    'dist': tf.io.FixedLenFeature([], tf.string)
-  }
+  def map(bin: SymbolicTensor) -> (SymbolicTensor, SymbolicTensor):
+    config = {
+      'data': tf.io.FixedLenFeature([], tf.string),
+      'dist': tf.io.FixedLenFeature([], tf.string)
+    }
 
-  # parsing example and tensor
-  parsed_example = tf.io.parse_example(bin, config)
-  data = tf.io.parse_tensor(parsed_example['data'], tf.float32)
+    # parsing example and tensor
+    parsed_example = tf.io.parse_example(bin, config)
+    if not three_channel:
+      data = tf.io.parse_tensor(parsed_example['data'], tf.float32)[:,0:1,:]
+    else:
+      data = tf.io.parse_tensor(parsed_example['data'], tf.float32)[:,:,:]
 
-  # transposing BxCxW to BxWxC (B: Batch Size, W: Window Size, C: Channel) 
-  data = tf.transpose(data, perm=[0, 2, 1])
+    # transposing BxCxW to BxWxC (B: Batch Size, W: Window Size, C: Channel) 
+    data = tf.transpose(data, perm=[0, 2, 1])
 
-  # parsing serialized epicentral distance data
-  dist = tf.io.parse_tensor(parsed_example['dist'], tf.float32)
-  dist = tf.expand_dims(dist, axis=-1)
+    # parsing serialized epicentral distance data
+    dist = tf.io.parse_tensor(parsed_example['dist'], tf.float32)
+    dist = tf.expand_dims(dist, axis=-1)
 
-  return (data, dist)
+    return (data, dist)
+  return map
 
-def reader_get_data_and_epicenter(tfrecords_dir: list) -> _UnbatchDataset:
+def reader_get_data_and_epicenter(tfrecords_dir: list, three_channel: bool) -> _UnbatchDataset:
   # enforcing the user to comply with the predefined data type
   Assertor().enforce_static_writing(reader_get_data_and_epicenter, locals())
 
-  return tf.data.TFRecordDataset(tfrecords_dir).map(_map_reader).unbatch()
+  return tf.data.TFRecordDataset(tfrecords_dir).map(_map_reader(three_channel)).unbatch()
